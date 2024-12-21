0d4808d9e65441b4565f2d02a3854f8042e39c14
pkjha1 <prashantcool28@gmail.com>
2024-12-21 19:10:35 +0000

    added theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "styles/tailwind.css";
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import "@radix-ui/themes/styles.css";
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
