732ff859712d0265dd72765cb7635d6553249279
pkjha1 <prashantcool28@gmail.com>
2024-12-21 19:17:13 +0000

    added styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,9 @@ import "@radix-ui/themes/styles.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Theme>{children}</Theme>
+        </body>
       <Analytics />
       <SpeedInsights/>
     </html>
